7cb7b387c88e2a98facd3d10938b139b5e9b165f
Benjamin Magee <bmagee@barracuda.com>
2020-11-21 15:21:52 -0500

    Update intcode.py to day 5's definition of the intcode program

--- intcode.py
+++ intcode.py
@@ -1,11 +1,56 @@
+def get_arg(index, pos):
+    mode = (program[index] // (10 ** (pos + 1))) % 10
+    if mode == 0:
+        return program[program[index+pos]]
+    elif mode == 1:
+        return program[index+pos]
+
 def intcode(program):
     index = 0
     while program[index] != 99:
-        op, arg1, arg2, save = program[index:index+4]
-        if op == 1:
-            program[save] = program[arg1] + program[arg2]
-        elif op == 2:
-            program[save] = program[arg1] * program[arg2]
+        op = program[index] % 100
+        if op in (1, 2, 7, 8):
+            arg1 = get_arg(index, 1)
+            arg2 = get_arg(index, 2)
+            arg3 = program[index+3]
+
+            if op == 1:
+                program[arg3] = int(arg1) + int(arg2)
+            elif op == 2:
+                program[arg3] = arg1 * arg2
+            elif op == 7:
+                if arg1 < arg2:
+                    program[arg3] = 1
+                else:
+                    program[arg3] = 0
+            elif op == 8:
+                if arg1 == arg2:
+                    program[arg3] = 1
+                else:
+                    program[arg3] = 0
+            index += 4
+        elif op == 3 or op ==4:
+            if op == 3:
+                program[program[index+1]] = input("Provide an integer: ")
+            elif op == 4:
+                arg1 = get_arg(index, 1)
+                print(str(arg1))
+            index += 2
+        elif op == 5 or op == 6:
+            arg1 = get_arg(index, 1)
+            arg2 = get_arg(index, 2)
+            if op == 5:
+                if arg1 != 0:
+                    index = arg2
+                else:
+                    index += 3
+            elif op == 6:
+                if arg1 == 0:
+                    index = arg2
+                else:
+                    index += 3
         elif op == 99:
             break
-        index += 4
+        else:
+            print("Unknown opcode")
+            break
