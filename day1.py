c3b72176960ee921441b640fde9137b2d1312ab6
Benjamin Magee <bmagee@barracuda.com>
2020-11-21 14:15:21 -0500

    Update day1.py to take file args

--- day1.py
+++ day1.py
@@ -1,11 +1,18 @@
+#!/usr/bin/python
+
+import argparse
 import math
 import sys
 
 def fuel(mass):
     return math.floor(mass / 3) - 2
 
+parser = argparse.ArgumentParser("Parse optional file inputs")
+parser.add_argument("-i", "--input", nargs="?", type=argparse.FileType("r"), default=sys.stdin)
+args = parser.parse_args()
+
 sum = 0
-for line in sys.stdin:
+for line in args.input:
     sum += fuel(int(line))
 
 print("Fuel: " + str(sum))
