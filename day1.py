180bef84561529d3076d13acba07e81d97d9aeb3
Benjamin Magee <bmagee@barracuda.com>
2020-11-21 14:22:21 -0500

    Add --recursive option to satisfy day 1 part 2

--- day1.py
+++ day1.py
@@ -4,12 +4,20 @@ import argparse
 import math
 import sys
 
-def fuel(mass):
-    return math.floor(mass / 3) - 2
-
 parser = argparse.ArgumentParser("Parse optional file inputs")
 parser.add_argument("-i", "--input", nargs="?", type=argparse.FileType("r"), default=sys.stdin)
+parser.add_argument("-r", "--recursive", action="store_true")
 args = parser.parse_args()
+
+def fuel(mass):
+    ret = math.floor(mass / 3) - 2
+    if args.recursive:
+        if ret <= 0:
+            return 0
+        else:
+            return ret + fuel(ret)
+    else:
+        return ret
 
 sum = 0
 for line in args.input:
